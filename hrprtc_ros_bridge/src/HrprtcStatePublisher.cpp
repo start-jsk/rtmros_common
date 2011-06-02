af3b0c467d41dfe35713b73d160048388782495d
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-02 08:36:21 +0000

    add hrprtc ros bridge

---
+++ hrprtc_ros_bridge/src/HrprtcStatePublisher.cpp
@@ -0,0 +1,238 @@
+// -*- C++ -*-
+/*!
+ * @file  HrprtcStatePublisher.cpp * @brief HrprtcState component * $Date$ 
+ *
+ * $Id$ 
+ */
+#include "HrprtcStatePublisher.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* hrprtcstatepublisher_spec[] =
+  {
+    "implementation_id", "HrprtcStatePublisher",
+    "type_name",         "HrprtcStatePublisher",
+    "description",       "HrprtcState component",
+    "version",           "0.1",
+    "vendor",            "JSK",
+    "category",          "Generic",
+    "activity_type",     "SPORADIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "10",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    ""
+  };
+// </rtc-template>
+
+HrprtcStatePublisher::HrprtcStatePublisher(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_in_rsangleIn("in_rsangle", m_in_rsangle),
+    m_in_mcangleIn("in_mcangle", m_in_mcangle),
+    m_in_rsrfsensorIn("in_rsrfsensor", m_in_rsrfsensor),
+    m_in_rslfsensorIn("in_rslfsensor", m_in_rslfsensor),
+    m_in_rsrhsensorIn("in_rsrhsensor", m_in_rsrhsensor),
+    m_in_rslhsensorIn("in_rslhsensor", m_in_rslhsensor),
+    m_in_gsensorIn("in_gsensor", m_in_gsensor),
+    m_in_gyrometerIn("in_gyrometer", m_in_gyrometer),
+    m_StateHolderServicePort("StateHolderService")
+
+    // </rtc-template>
+{
+}
+
+HrprtcStatePublisher::~HrprtcStatePublisher()
+{
+}
+
+
+RTC::ReturnCode_t HrprtcStatePublisher::onInitialize()
+{
+  std::cerr << "@Initilize name : " << getInstanceName() << std::endl;
+  joint_state_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
+
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("in_rsangle", m_in_rsangleIn);
+  addInPort("in_mcangle", m_in_mcangleIn);
+  addInPort("in_rsrfsensor", m_in_rsrfsensorIn);
+  addInPort("in_rslfsensor", m_in_rslfsensorIn);
+  addInPort("in_rsrhsensor", m_in_rsrhsensorIn);
+  addInPort("in_rslhsensor", m_in_rslhsensorIn);
+  addInPort("in_gsensor", m_in_gsensorIn);
+  addInPort("in_gyrometer", m_in_gyrometerIn);
+
+  // Set OutPort buffer
+
+  // Set service provider to Ports
+
+  // Set service consumers to Ports
+  m_StateHolderServicePort.registerConsumer("service0", "StateHolderService", m_service0);
+
+  // Set CORBA Service Ports
+  addPort(m_StateHolderServicePort);
+
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+
+  // </rtc-template>
+
+  RTC::Properties& prop = getProperties();
+  body = new hrp::Body();
+  std::string nameServer = m_pManager->getConfig ()["corba.nameservers"];
+  int comPos = nameServer.find (",");
+  if (comPos < 0)
+    {
+      comPos = nameServer.length();
+    }
+  nameServer = nameServer.substr(0, comPos);
+  std::cerr << nameServer.c_str() << std::endl;
+  RTC::CorbaNaming naming(m_pManager->getORB(), nameServer.c_str());
+  CosNaming::NamingContext::_duplicate(naming.getRootContext());
+  try  {
+    std::cerr << "Loading " << prop["model"].c_str () << std::endl;
+    loadBodyFromModelLoader (body,
+			     "/opt/grx/HRP2JSK/model/HRP2JSKmain.wrl",
+			     //"/opt/grx/HIRONX/model/main.wrl",
+			     CosNaming::NamingContext::_duplicate(naming.getRootContext()));
+  } catch ( CORBA::SystemException& ex ) {
+    std::cerr << "Loaded " << body->name() << std::endl;
+    // this id OK
+  } catch ( ... ) {
+      std::cerr << "failed to load model[" << prop["model"] << "]" << std::endl;
+      return RTC::RTC_ERROR;
+  }
+  body->calcForwardKinematics();
+  return RTC::RTC_OK;
+}
+
+
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+RTC::ReturnCode_t HrprtcStatePublisher::onExecute(RTC::UniqueId ec_id)
+{
+  std::cerr << "@Execute name : " << getInstanceName() << std::endl;
+  // m_in_rsangleIn
+  if ( m_in_rsangleIn.isNew () ) {
+    try {
+      m_in_rsangleIn.read();
+    }
+    catch(const std::runtime_error &e)
+      {
+	std::cerr << e.what() << std::endl;
+      }
+    //
+    sensor_msgs::JointState joint_state;
+    joint_state.header.stamp = ros::Time::now();
+    m_in_rsangleIn.read();
+
+    body->calcForwardKinematics();
+    std::cerr << body->name() << std::endl;
+    if ( m_in_rsangle.data.length() != body->joints().size() ) {
+      std::cerr << "rsangle.data.length(" << m_in_rsangle.data.length() << ") is not equal to body->joints().length(" << body->joints().size() << ")" << std::endl;
+      return RTC::RTC_ERROR;
+    }
+    body->calcForwardKinematics();
+
+    for ( unsigned int i = 0; i < m_in_rsangle.data.length() ; i++ ){
+      body->joint(i)->q = m_in_rsangle.data[i];
+      std::cerr << m_in_rsangle.data[i] << " ";
+    }
+    std::cerr << std::endl;
+    std::vector<hrp::Link*>::const_iterator it = body->joints().begin();
+    while ( it != body->joints().end() ) {
+      hrp::Link* j = ((hrp::Link*)*it);
+      std::cout << j->name << " - " << j->q << std::endl;
+      joint_state.name.push_back(j->name);
+      joint_state.position.push_back(j->q);
+      //joint_state.velocity
+      //joint_state.effort
+      ++it;
+    }
+    joint_state_pub.publish(joint_state);
+    ros::spinOnce();
+  }
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t HrprtcStatePublisher::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+extern "C"
+{
+ 
+  void HrprtcStatePublisherInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(hrprtcstatepublisher_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<HrprtcStatePublisher>,
+                             RTC::Delete<HrprtcStatePublisher>);
+  }
+  
+};
+
+
+
---
+++ hrprtc_ros_bridge/src/HrprtcStatePublisherComp.cpp
@@ -0,0 +1,277 @@
+// -*- C++ -*-
+/*!
+ * @file HrprtcStatePublisherComp.cpp
+ * @brief Standalone component
+ * @date $Date$ 
+ *
+ * $Id$ 
+ */
+#include <tvmet/Matrix.h>
+#include <rtm/CorbaNaming.h>
+#include <rtm/Manager.h>
+#include <iostream>
+#include <string>
+#include "HrprtcStatePublisher.h"
+
+PortService_var getPort(RTC::RTObject_var rtobj, const char *port_name) {
+  // find name comp::get_ports :key #'(lambda (x) (send x :name))
+
+  RTC::PortServiceList* portlist;
+  portlist = rtobj->get_ports();
+
+  for (CORBA::ULong i(0), n(portlist->length()); i < n; ++i)
+    {
+      PortService_var port;
+      port = (*portlist)[i];
+      if ( strcmp(strrchr(port->get_port_profile()->name,'.')+1, port_name) == 0
+	   ) {
+	std::cout << "Port" << i << " (name): ";
+	std::cout << port->get_port_profile()->name << std::endl;
+        return port;
+      }
+    }
+
+  // std::cerr
+  std::cerr << "not found error" << std::endl;
+  return NULL;
+}
+
+void ConnectPorts(RTC::PortService_var pout, RTC::PortService_var pin)
+{
+  // connect ports: copy from simpleIO/ConnectorComp.cpp
+  std::string subs_type("flush");
+  ConnectorProfile prof;
+  std::string period;
+  std::string push_policy;
+  std::string skip_count;
+  std::string endian;
+  std::string connect_origin("in");
+  std::map<std::string, std::string> buffer_prop;
+
+  prof.connector_id = "";
+  prof.name = CORBA::string_dup("connector0");
+  prof.ports.length(2);
+  prof.ports[0] = pin;
+  prof.ports[1] = pout;
+  CORBA_SeqUtil::push_back(prof.properties,
+                           NVUtil::newNV("dataport.interface_type",
+                                         "corba_cdr"));
+  CORBA_SeqUtil::push_back(prof.properties,
+                           NVUtil::newNV("dataport.dataflow_type",
+                                         "push"));
+  if (subs_type != "")
+    CORBA_SeqUtil::push_back(prof.properties,
+			     NVUtil::newNV("dataport.subscription_type",
+					   subs_type.c_str()));
+  else
+    CORBA_SeqUtil::push_back(prof.properties,
+			     NVUtil::newNV("dataport.subscription_type",
+					   "flush"));
+  if (subs_type == "periodic" && period != "")
+    CORBA_SeqUtil::push_back(prof.properties,
+			     NVUtil::newNV("dataport.publisher.push_rate",
+					   period.c_str()));
+  if (push_policy != "")
+    CORBA_SeqUtil::push_back(prof.properties,
+			     NVUtil::newNV("dataport.publisher.push_policy",
+					   push_policy.c_str()));
+  if (push_policy == "skip" && skip_count != "")
+    CORBA_SeqUtil::push_back(prof.properties,
+			     NVUtil::newNV("dataport.publisher.skip_count",
+					   skip_count.c_str()));
+  if (endian != "")
+    CORBA_SeqUtil::push_back(prof.properties,
+			     NVUtil::newNV("dataport.serializer.cdr.endian",
+					   endian.c_str()));
+
+  std::map<std::string, std::string>::iterator it=buffer_prop.begin();
+  while (it != buffer_prop.end()) {
+    std::string key("dataport.");
+    key += it->first;
+    CORBA_SeqUtil::push_back(prof.properties,
+                             NVUtil::newNV(key.c_str(),
+                                           it->second.c_str()));
+    ++it;
+  }
+
+  ReturnCode_t ret;
+  if (connect_origin == "in")
+    ret = pin->connect(prof);
+  else
+    ret = pout->connect(prof);
+
+  assert(ret == RTC::RTC_OK);
+
+  std::cerr << "Connector " << pout->get_port_profile()->name << " -> " << pin->get_port_profile()->name << " ID: " << prof.connector_id << std::endl;
+  //NVUtil::dump(prof.properties);
+
+  // done connect
+  return;
+}
+
+void MyModuleInit(RTC::Manager* manager)
+{
+  HrprtcStatePublisherInit(manager);
+  RTC::RtcBase* comp;
+
+  // Create a component
+  comp = manager->createComponent("HrprtcStatePublisher");
+
+  //
+  RTC::ComponentProfile_var prof;
+  prof = comp->get_component_profile();
+  std::cout << "=================================================" << std::endl;
+  std::cout << " Component Profile" << std::endl;
+  std::cout << "-------------------------------------------------" << std::endl;
+  std::cout << "InstanceID:     " << prof->instance_name << std::endl;
+  std::cout << "Implementation: " << prof->type_name << std::endl;
+  std::cout << "Description:    " << prof->description << std::endl;
+  std::cout << "Version:        " << prof->version << std::endl;
+  std::cout << "Maker:          " << prof->vendor << std::endl;
+  std::cout << "Category:       " << prof->category << std::endl;
+  std::cout << "  Other properties   " << std::endl;
+  NVUtil::dump(prof->properties);
+  std::cout << "=================================================" << std::endl;
+
+  PortServiceList* portlist;
+  portlist = comp->get_ports();
+
+  for (CORBA::ULong i(0), n(portlist->length()); i < n; ++i)
+    {
+      PortService_ptr port;
+      port = (*portlist)[i];
+      std::cout << "================================================="
+                << std::endl;
+      std::cout << "Port" << i << " (name): ";
+      std::cout << port->get_port_profile()->name << std::endl;
+      std::cout << "-------------------------------------------------"
+                << std::endl;
+      RTC::PortInterfaceProfileList iflist;
+      iflist = port->get_port_profile()->interfaces;
+
+      for (CORBA::ULong i(0), n(iflist.length()); i < n; ++i)
+        {
+	  std::cout << "I/F name: ";
+	  std::cout << iflist[i].instance_name << std::endl;
+	  std::cout << "I/F type: ";
+	  std::cout << iflist[i].type_name << std::endl;
+          const char* pol;
+          pol = iflist[i].polarity == 0 ? "PROVIDED" : "REQUIRED";
+	  std::cout << "Polarity: " << pol << std::endl;
+        }
+      std::cout << "- properties -" << std::endl;
+      NVUtil::dump(port->get_port_profile()->properties);
+      std::cout << "-------------------------------------------------" << std::endl;
+    }
+#if 0
+  // Example
+  // The following procedure is examples how handle RT-Components.
+  // These should not be in this function.
+  // Get the component's object reference
+  RTC::RTObject_var rtobj;
+  rtobj = RTC::RTObject::_narrow(manager->getPOA()->servant_to_reference(comp));
+
+  //
+  std::string port_no("2809");
+  CORBA::ORB_ptr orb = manager->getORB();
+  //std::string name_server("localhost:");
+  std::string name_server("192.168.127.132:");
+  name_server.append(port_no);
+  RTC::CorbaNaming naming(orb, name_server.c_str());
+
+  RTC::RTObject_var rhobj, shobj;
+  //
+  CorbaConsumer<RTC::RTObject> rh;
+  try{
+    rh.setObject(naming.resolve("HRP2JSKController(Robot)0.rtc"));
+    //rh.setObject(naming.resolve("RobotHardware0.rtc"));
+  } catch (...) {
+    std::cerr << "err : controller holder" << std::endl;
+    return;
+  }
+  rhobj = rh._ptr();
+  //
+  CorbaConsumer<RTC::RTObject> sh;
+  try {
+    sh.setObject(naming.resolve("StateHolder0.rtc"));
+  } catch (...) {
+    std::cerr << "err : state holder" << std::endl;
+    return;
+  }
+  shobj = sh._ptr();
+  ConnectPorts(getPort(shobj, "qOut"), getPort(rtobj, "in_rsangle"));
+
+  // execute
+  ExecutionContext_ptr ec = rtobj->get_context(0);
+  ec->start();
+  ec->activate_component(rtobj);
+#endif
+  // Example
+  // The following procedure is examples how handle RT-Components.
+  // These should not be in this function.
+
+  // Get the component's object reference
+//  RTC::RTObject_var rtobj;
+//  rtobj = RTC::RTObject::_narrow(manager->getPOA()->servant_to_reference(comp));
+
+  // Get the port list of the component
+//  PortList* portlist;
+//  portlist = rtobj->get_ports();
+
+  // getting port profiles
+//  std::cout << "Number of Ports: ";
+//  std::cout << portlist->length() << std::endl << std::endl; 
+//  for (CORBA::ULong i(0), n(portlist->length()); i < n; ++i)
+//  {
+//    Port_ptr port;
+//    port = (*portlist)[i];
+//    std::cout << "Port" << i << " (name): ";
+//    std::cout << port->get_port_profile()->name << std::endl;
+//    
+//    RTC::PortInterfaceProfileList iflist;
+//    iflist = port->get_port_profile()->interfaces;
+//    std::cout << "---interfaces---" << std::endl;
+//    for (CORBA::ULong i(0), n(iflist.length()); i < n; ++i)
+//    {
+//      std::cout << "I/F name: ";
+//      std::cout << iflist[i].instance_name << std::endl;
+//      std::cout << "I/F type: ";
+//      std::cout << iflist[i].type_name << std::endl;
+//      const char* pol;
+//      pol = iflist[i].polarity == 0 ? "PROVIDED" : "REQUIRED";
+//      std::cout << "Polarity: " << pol << std::endl;
+//    }
+//    std::cout << "---properties---" << std::endl;
+//    NVUtil::dump(port->get_port_profile()->properties);
+//    std::cout << "----------------" << std::endl << std::endl;
+//  }
+
+  return;
+}
+
+int main (int argc, char** argv)
+{
+  RTC::Manager* manager;
+  manager = RTC::Manager::init(argc, argv);
+  ros::init(argc, argv, "hrprtc_state_publisher", ros::init_options::NoSigintHandler);
+
+  // Initialize manager
+  manager->init(argc, argv);
+
+  // Set module initialization proceduer
+  // This procedure will be invoked in activateManager() function.
+  manager->setModuleInitProc(MyModuleInit);
+
+  // Activate manager and register to naming service
+  manager->activateManager();
+
+  // run the manager in blocking mode
+  // runManager(false) is the default.
+  manager->runManager();
+
+  // If you want to run the manager in non-blocking mode, do like this
+  // manager->runManager(true);
+
+  return 0;
+}
+
