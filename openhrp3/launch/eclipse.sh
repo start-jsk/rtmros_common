dcbd5dfcee82cf6ff54b955f9543a019be582b3c
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-03 04:22:12 +0000

    add PATH to openhrp3/lib so that eclipse is able to find openhrp-xxx-servers

--- openhrp3/launch/eclipse.sh
+++ openhrp3/launch/eclipse.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export PROJECT_DIR=`rospack find openhrp3`/share/OpenHRP-3.1/sample/project
+export PATH=`rospack find openhrp3`/bin:$PATH
 export LD_LIBRARY_PATH=`rospack find openhrp3`/lib:$LD_LIBRARY_PATH
+
 # java3d
 export CLASSPATH=.:$CLASSPATH:`rospack find openhrp3`/lib/ext/j3dcore.jar:`rospack find openhrp3`/lib/ext/j3dutils.jar:`rospack find openhrp3`/lib/ext/vecmath.jar
 export LD_LIBRARY_PATH=`rospack find openhrp3`/lib/amd64:$LD_LIBRARY_PATH
