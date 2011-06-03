b1375c74c7d400ed50c5dbf8582c2a4045a826c2
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-03 09:52:16 +0000

    support to pass project.xml file to eclipse.sh

--- openhrp3/launch/eclipse.sh
+++ openhrp3/launch/eclipse.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ "$1" != "" ]; then
+    export INITIAL_PROJECT=$1
+fi
 export PROJECT_DIR=`rospack find openhrp3`/share/OpenHRP-3.1/sample/project
 export PATH=`rospack find openhrp3`/bin:$PATH
 export LD_LIBRARY_PATH=`rospack find openhrp3`/lib:$LD_LIBRARY_PATH
@@ -7,6 +10,7 @@ export LD_LIBRARY_PATH=`rospack find openhrp3`/lib:$LD_LIBRARY_PATH
 export CLASSPATH=.:$CLASSPATH:`rospack find openhrp3`/lib/ext/j3dcore.jar:`rospack find openhrp3`/lib/ext/j3dutils.jar:`rospack find openhrp3`/lib/ext/vecmath.jar
 export LD_LIBRARY_PATH=`rospack find openhrp3`/lib/amd64:$LD_LIBRARY_PATH
 export LANG=C
+echo "initialProjectFile=${INITIAL_PROJECT}" >> `rospack find openhrp3`/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/com.generalrobotix.ui.grxui.prefs
 eclipse -data `rospack find openhrp3`/workspace -consolelog -clean \
 	-application com.generalrobotix.ui.grxui.applications \
         -perspective com.generalrobotix.ui.grxui.GrxUIPerspectiveFactory
