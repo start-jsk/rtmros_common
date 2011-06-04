313c09c2d73bb77fa32122df8223c4e3fae37b2a
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-04 04:05:33 +0000

    clean up script

--- openhrp3/launch/grxui.sh
+++ openhrp3/launch/grxui.sh
@@ -9,7 +9,10 @@ export LD_LIBRARY_PATH=`rospack find openhrp3`/lib:$LD_LIBRARY_PATH
 # java3d
 export CLASSPATH=.:$CLASSPATH:`rospack find openhrp3`/lib/ext/j3dcore.jar:`rospack find openhrp3`/lib/ext/j3dutils.jar:`rospack find openhrp3`/lib/ext/vecmath.jar
 export LD_LIBRARY_PATH=`rospack find openhrp3`/lib/amd64:$LD_LIBRARY_PATH
+
+#
 export LANG=C
+export GDK_NATIVE_WINDOWS=1
 echo "initialProjectFile=${INITIAL_PROJECT}" >> `rospack find openhrp3`/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/com.generalrobotix.ui.grxui.prefs
 eclipse -data `rospack find openhrp3`/workspace -consolelog -clean \
 	-application com.generalrobotix.ui.grxui.applications \
