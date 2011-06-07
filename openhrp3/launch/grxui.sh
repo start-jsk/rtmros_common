fea8ba0fbb4d5dba832552a1ebcee44199e45206
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-07 08:03:57 +0000

    split grxui into grxui and eclipse

--- openhrp3/launch/grxui.sh
+++ openhrp3/launch/grxui.sh
@@ -2,20 +2,7 @@
 if [ "$1" != "" ]; then
     export INITIAL_PROJECT=$1
 fi
-export PROJECT_DIR=`rospack find openhrp3`/share/OpenHRP-3.1/sample/project
-export PATH=`rospack find openhrp3`/bin:$PATH
-export LD_LIBRARY_PATH=`rospack find openhrp3`/lib:$LD_LIBRARY_PATH
 
-# java3d
-export CLASSPATH=.:$CLASSPATH:`rospack find openhrp3`/lib/ext/j3dcore.jar:`rospack find openhrp3`/lib/ext/j3dutils.jar:`rospack find openhrp3`/lib/ext/vecmath.jar
-export LD_LIBRARY_PATH=`rospack find openhrp3`/lib/amd64:$LD_LIBRARY_PATH
-
-#
-export LANG=C
-export GDK_NATIVE_WINDOWS=1
-echo "initialProjectFile=${INITIAL_PROJECT}" >> `rospack find openhrp3`/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/com.generalrobotix.ui.grxui.prefs
-eclipse -data `rospack find openhrp3`/workspace -consolelog -clean \
+rosrun openhrp3 eclipse.sh \
 	-application com.generalrobotix.ui.grxui.applications \
         -perspective com.generalrobotix.ui.grxui.GrxUIPerspectiveFactory
-
-
