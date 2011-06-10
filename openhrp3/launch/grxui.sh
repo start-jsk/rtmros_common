97657f3217b48bce955d423bfb6e1a338de02082
chen.jsk <chen.jsk@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-10 03:49:03 +0000

    forse restart omniNames

--- openhrp3/launch/grxui.sh
+++ openhrp3/launch/grxui.sh
@@ -3,6 +3,8 @@ if [ "$1" != "" ]; then
     export INITIAL_PROJECT=$1
 fi
 
+sudo pkill -KILL omniNames
+rtm-naming
 rosrun openhrp3 eclipse.sh \
 	-application com.generalrobotix.ui.grxui.applications \
         -perspective com.generalrobotix.ui.grxui.GrxUIPerspectiveFactory
