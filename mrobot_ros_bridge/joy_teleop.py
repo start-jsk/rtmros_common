f5aa72adc6886bb66cbef3734e95eeb89b1873b0
h.mikita89 <h.mikita89@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-06-21 15:01:03 +0000

    change speed parameter

--- mrobot_ros_bridge/joy_teleop.py
+++ mrobot_ros_bridge/joy_teleop.py
@@ -22,9 +22,9 @@ def joy_cb(msg):
             sp = 0.25
         tws.linear = geometry_msgs.msg.Vector3(ax[3] * sp,0,0)
         if tws.linear.x < 0:
-            tws.angular = geometry_msgs.msg.Vector3(0,0,-1.0 * (ax[0] + ax[2]) * math.pi * 0.6 * sp)
+            tws.angular = geometry_msgs.msg.Vector3(0,0,-1.0 * (ax[0] + ax[2]) * math.pi * 0.9 * sp)
         else:
-            tws.angular = geometry_msgs.msg.Vector3(0,0,(ax[0] + ax[2]) * math.pi * 0.6 * sp)
+            tws.angular = geometry_msgs.msg.Vector3(0,0,(ax[0] + ax[2]) * math.pi * 0.9 * sp)
     elif len(but) == 11 and but[3] > 0: # wii
         tws.linear  =  geometry_msgs.msg.Vector3((but[8]-but[9]),0,0)
         tws.angular = geometry_msgs.msg.Vector3(0,0,(but[6]-but[7]) * math.pi * 0.4)
