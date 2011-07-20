96d289025fd01c4d9101c31189148a1758caac69
manabu1118 <manabu1118@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-07-20 17:18:19 +0000

    add OpenHRP controller for beego

---
+++ mrobot_ros_bridge/src/IISBeegoController.cpp
@@ -0,0 +1,201 @@
+// -*- C++ -*-
+/*!
+ * @file  IISBeegoController.cpp * @brief IIS Beego Controller * $Date$ 
+ *
+ * $Id$ 
+ */
+#include "IISBeegoController.h"
+#include <math.h> // for M_PI
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* iisbeegocontroller_spec[] =
+  {
+    "implementation_id", "IISBeegoController",
+    "type_name",         "IISBeegoController",
+    "description",       "IIS",
+    "version",           "1.0.0",
+    "vendor",            "JSK",
+    "category",          "OpenHRP Controller",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    ""
+  };
+// </rtc-template>
+
+IISBeegoController::IISBeegoController(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_angleIn("angle", m_angle),
+    m_velocityIn("velocity", m_velocity),
+    m_torqueOut("torque", m_torque),
+    m_inIn("in", m_in),
+    m_outOut("out", m_out)
+
+    // </rtc-template>
+{
+}
+
+IISBeegoController::~IISBeegoController()
+{
+}
+
+
+RTC::ReturnCode_t IISBeegoController::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+
+  addInPort("angle", m_angleIn);
+  addInPort("velocity", m_velocityIn);
+  addInPort("in", m_inIn);
+
+  // Set OutPort buffer
+  addOutPort("torque", m_torqueOut);
+  addOutPort("out", m_outOut);
+
+  // Set service provider to Ports
+
+  // Set service consumers to Ports
+
+  // Set CORBA Service Ports
+
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+
+  // </rtc-template>
+  m_angle.data.length(1);
+  m_angle.data[0] = 0.0;
+  m_velocity.data.length(2);
+  m_velocity.data[0] = m_velocity.data[1] = 0.0;
+  m_torque.data.length(2);
+  return RTC::RTC_OK;
+}
+
+
+/*
+RTC::ReturnCode_t IISBeegoController::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+RTC::ReturnCode_t IISBeegoController::onExecute(RTC::UniqueId ec_id)
+{
+  static double tiresize = 0.04; // [m] TODO
+  static double tread = 0.25; // [m] TODO
+
+  if ( m_angleIn.isNew() &&
+       m_velocityIn.isNew() ) {
+    m_angleIn.read();
+    m_velocityIn.read();
+    double l_tireVel = m_velocity.data[0] * (tiresize / 2.0 / M_PI);
+    double r_tireVel = m_velocity.data[1] * (tiresize / 2.0 / M_PI);
+    fprintf(stderr, "[simulate] l tire vel = %.3f, r tire vel = %.3f\n", l_tireVel, r_tireVel);
+
+    static double LeftCommandVel = 0, RightCommandVel = 0;
+    if ( m_inIn.isNew() ) {
+
+      // read data from client
+      m_inIn.read();
+
+      //
+	  LeftCommandVel  = (2.0*M_PI/tiresize) * (m_in.vx - m_in.w*tread/2);
+	  RightCommandVel = (2.0*M_PI/tiresize) * (m_in.vx + m_in.w*tread/2);
+      fprintf(stderr, "[command]  x = %.3f y = %.3f, w = %.3f\n", m_in.vx, m_in.vy, m_in.w);
+    }
+    double LeftTireTorque = (LeftCommandVel * 10 - l_tireVel);
+    double RightTireTorque = (RightCommandVel * 10 - r_tireVel);
+
+    // write to simulated robot
+    m_torque.data[0] = LeftTireTorque;
+    m_torque.data[1] = RightTireTorque;
+    m_torqueOut.write();
+
+    m_out.x = (l_tireVel + r_tireVel) / 2;
+    m_out.y = 0;
+    m_out.theta = (r_tireVel - l_tireVel) / tread;
+    m_outOut.write();
+  }
+
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t IISBeegoController::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+/*
+RTC::ReturnCode_t IISBeegoController::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+extern "C"
+{
+ 
+  void IISBeegoControllerInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(iisbeegocontroller_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<IISBeegoController>,
+                             RTC::Delete<IISBeegoController>);
+  }
+  
+};
+
+
+
