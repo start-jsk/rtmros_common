8c5f564bb1776c8ab8ca0c840103129416524a8a
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-11-03 07:21:12 +0000

    fix to actually svn up and svn merge

--- jenkins_latest.sh
+++ jenkins_latest.sh
@@ -17,22 +17,22 @@ latest_uri=https://rtm-ros-robotics.googlecode.com/svn/tags/latest/rtmros_common
 latest=$WORKSPACE/rtmros_common
 target=$HOME/jobs/agentsystem/workspace/rtm-ros-robotics-$BUILD_NUMBER/rtmros_common
 
-#rm -fr rtmros_common ; svn co $latest_uri rtmros_common;
+rm -fr rtmros_common ; svn co $latest_uri rtmros_common;
 
 latest_revision=`python -c "import pysvn; print pysvn.Client().info('$latest').commit_revision.number"`
 target_revision=`python -c "import pysvn; print pysvn.Client().info('$target').commit_revision.number"`
-echo ";; latest revision : $latest_revision" 
-echo ";; target revision : $target_revision" 
+echo ";; latest revision : $latest_revision"
+echo ";; target revision : $target_revision"
 
-#(cd $latest; svn merge -r $latest_revision:$target_revision $target) 
+(cd $latest; svn merge -r $latest_revision:$target_revision $target)
 export ROS_PACKAGE_PATH=$latest:$ROS_PACKAGE_PATH
 sed -i s/^SVN_REVISION=-r.*$/SVN_REVISION=-r$hrpsys_revision/ `rospack find hrpsys`/Makefile.hrpsys-base
 sed -i s/^HG_REVISION=.*$/HG_REVISION=$openhrp3_revision/ `rospack find openhrp3`/Makefile
 
-cat <<EOF >  latest.commit.msg 
+cat <<EOF >  latest.commit.msg
 Add latest tag for $target_revision by Jenkins
 Update from previous latest version($latest_revision) are....
 
 `svn log -r $latest_revision:$target_revision $target`
 EOF
-svn commit --non-interactive --username rtmrosrobotics.testing@gmail.com --password XC6HC3Jy2FG3 -F latest.commit.msg $latest 
+svn commit --non-interactive --username rtmrosrobotics.testing@gmail.com --password XC6HC3Jy2FG3 -F latest.commit.msg $latest
