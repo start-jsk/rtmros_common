21ae0fada0b91ad3609b7c2d7370368b77afc013
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-11-11 17:19:25 +0000

    make sure to kill eclipse

--- openhrp3/test/sample-projects.sh
+++ openhrp3/test/sample-projects.sh
@@ -37,6 +37,11 @@ EOF
     # start openhrp3
     rosrun openhrp3 openhrp-shutdown-servers
     rosrun openrtm rtm-naming-restart &
+    # kill eclipse to avoid double start
+    ps -C eclipse && pkill -KILL eclipse
+    PID=`ps aux | grep /usr/lib/eclipse//plugins/org.eclipse.equinox.launcher_1.0.201.R35x_v20090715.jar | grep -v grep | awk '{print $2}'`
+    if [ ! "$PID" == "" ]; then kill -KILL $PID; fi
+    #
     echo "start $filename" 1>&2
     rosrun openhrp3 grxui.sh $filename > /dev/null &
     while :; do
