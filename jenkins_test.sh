ef7d284eedf714afa615428c2a39ddf812f238e2
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-12-03 11:33:00 +0000

    fix typo in jenkins_test.sh

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -18,7 +18,7 @@ test-grxui hrpsys
 # resize eclipse window size
 sed -i 's/height="768" width="1024"/height="723" width="506"/' `rospack find openhrp3`/workspace/.metadata/.plugins/org.eclipse.ui.workbench/workbench.xml
 # do test
-test-grxui hrpsys hrpsys_ros_bridge
+test-grxui hrpsys_ros_bridge
 
 # done
 rosrun rosunit clean_junit_xml.py
