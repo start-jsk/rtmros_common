d9a78a99743379140282c3566a4677bba75a55f2
Atsushi.tsuda@gmail.com <Atsushi.tsuda@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-01-26 07:38:24 +0000

    update script file for hrpsys

--- hrpsys/scripts/hrpsys-launch.sh
+++ hrpsys/scripts/hrpsys-launch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 ROBOT_NAME=`rosrun openhrp3 extract-robotname -ORBInitRef NameService=corbaloc:iiop:$RTCTREE_NAMESERVERS:2809/NameService $1`
-if [ $? != 0 ]; then exit 1; fi
 ROBOT_NAME=$ROBOT_NAME RTCTREE_NAMESERVERS=$RTCTREE_NAMESERVERS rosrun openrtm rtmlaunch.py $2
 
