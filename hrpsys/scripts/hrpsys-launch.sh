e9719d4f6b3243fbd0de62974614849950e19a6f
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-01-26 07:23:34 +0000

    fix to pass -ORBInitRef to extract-robotname

--- hrpsys/scripts/hrpsys-launch.sh
+++ hrpsys/scripts/hrpsys-launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROBOT_NAME=`rosrun openhrp3 extract-robotname $1` -ORBInitRef NameService=corbaloc:iiop:$RTCTREE_NAMESERVERS:2809/NameService
+ROBOT_NAME=`rosrun openhrp3 extract-robotname -ORBInitRef NameService=corbaloc:iiop:$RTCTREE_NAMESERVERS:2809/NameService $1`
 if [ $? != 0 ]; then exit 1; fi
 ROBOT_NAME=$ROBOT_NAME RTCTREE_NAMESERVERS=$RTCTREE_NAMESERVERS rosrun openrtm rtmlaunch.py $2
 
