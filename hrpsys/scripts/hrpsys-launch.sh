ea8230ca04698d45fdf1ec2ee1e1b64a9b60e55c
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-01-26 05:30:34 +0000

    suport RTCNAME_SERVERS

--- hrpsys/scripts/hrpsys-launch.sh
+++ hrpsys/scripts/hrpsys-launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROBOT_NAME=`rosrun openhrp3 extract-robotname $1`
+ROBOT_NAME=`rosrun openhrp3 extract-robotname $1` -ORBInitRef NameService=corbaloc:iiop:$RTCTREE_NAMESERVERS:2809/NameService
 if [ $? != 0 ]; then exit 1; fi
-ROBOT_NAME=$ROBOT_NAME rosrun openrtm rtmlaunch.py $2
+ROBOT_NAME=$ROBOT_NAME RTCTREE_NAMESERVERS=$RTCTREE_NAMESERVERS rosrun openrtm rtmlaunch.py $2
 
