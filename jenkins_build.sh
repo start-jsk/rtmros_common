344043d3762c91f460a2f5d1604d38d9395ccb18
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-02-13 03:05:15 +0000

    add --no-install option

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 usage_exit () {
-    echo "Usage : $0 [-t|--target TARGET_PACKAGE] ROS_DISTRIBUTION" 1>&2
+    echo "Usage : $0 [-t|--target TARGET_PACKAGE] [--no-install] ROS_DISTRIBUTION" 1>&2
     exit 1
 }
-trap usage ERR
+trap usage_exit ERR
 
 export LANG=C
 
 # command line parse
-GETOPT=`getopt -o ht: -l help,target: -- "$@"` ; [ $? != 0 ] && usage_exit
+GETOPT=`getopt -o ht: -l help,no-install,target: -- "$@"` ; [ $? != 0 ] && usage_exit
 
 TARGET=""
+INSTALL=1
 eval set -- "$GETOPT"
 while [ -n "$1" ] ; do
     case $1 in
         -t|--target) TARGET=$2; shift 2;;
         -h|--help) usage_exit ;;
+        --no-install) INSTALL=0; shift ;;
         --) shift; break;;
         *) echo "Unknown option($1)"; usage_exit;;
     esac
@@ -29,23 +31,26 @@ echo "Running $0 with {distribution:$DISTRIBUTION, target:$TARGET}"
 if [ "$WORKSPACE" == "" ]; then # if not jenkins
     export WORKSPACE=$HOME
 fi
-if [ "$ROS_INSTALLDIR" == "" ]; then # if not jenkins
+if [ "$ROS_INSTALLDIR" == "" ]; then
     export ROS_INSTALLDIR=$WORKSPACE/ros/$DISTRIBUTION
 fi
 
-# setup ros packages
-wget https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/jsk.rosbuild -O /tmp/jsk.rosbuild
-bash /tmp/jsk.rosbuild --show-install | sh
+if [ $INSTALL == 1 ]; then
 
-# rosinstall
-/usr/local/bin/rosinstall --rosdep-yes --continue-on-error  --delete-changed-uris $ROS_INSTALLDIR /opt/ros/$DISTRIBUTION  http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall || rosinstall $ROS_INSTALLDIR
+    # setup ros packages
+    wget https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/jsk.rosbuild -O /tmp/jsk.rosbuild
+    bash /tmp/jsk.rosbuild --show-install | sh
 
-# copy jenkins source
-if [ "$JENKINS_HOME" != "" ]; then #if jenkins
-    rm -fr $ROS_INSTALLDIR/rtm-ros-robotics/rtmros_common/
-    cp -r $WORKSPACE/rtm-ros-robotics/rtmros_common $ROS_INSTALLDIR/rtm-ros-robotics/rtmros_common
-    # set ROS_HOME under workspace so that we can check from web interface
-    echo "export ROS_HOME=$WORKSPACE/.ros" >> $ROS_INSTALLDIR/setup.sh
+    # rosinstall
+    /usr/local/bin/rosinstall --rosdep-yes --continue-on-error  --delete-changed-uris $ROS_INSTALLDIR /opt/ros/$DISTRIBUTION  http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall || rosinstall $ROS_INSTALLDIR
+
+    # copy jenkins source
+    if [ "$JENKINS_HOME" != "" ]; then #if jenkins
+	rm -fr $ROS_INSTALLDIR/rtm-ros-robotics/rtmros_common/
+	cp -r $WORKSPACE/rtm-ros-robotics/rtmros_common $ROS_INSTALLDIR/rtm-ros-robotics/rtmros_common
+	# set ROS_HOME under workspace so that we can check from web interface
+	echo "export ROS_HOME=$WORKSPACE/.ros" >> $ROS_INSTALLDIR/setup.sh
+    fi
 fi
 
 # source
