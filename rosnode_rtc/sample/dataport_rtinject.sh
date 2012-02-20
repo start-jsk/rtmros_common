b7edf88eea6fe3358b5bd8ce23a9ad77cbda3f88
manabu1118 <manabu1118@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-02-20 21:35:55 +0000

    add 2 images, node-rtc detaport figure

--- rosnode_rtc/sample/dataport_rtinject.sh
+++ rosnode_rtc/sample/dataport_rtinject.sh
@@ -2,15 +2,16 @@
 
 port=$1
 data=$2
+count=10
 
 echo "waiting..."
 while [ 0 -eq `rtcat ${port} | grep "+DataInPort" | wc -l` ]
 do
     sleep 1
 done
-echo "start injecting 10 times to ${port}"
+echo "start injecting ${count} times to ${port}"
 
-rosrun openrtm rtinject -p /tmp -m RTMROSDataBridge -n 10 -c "${data}" ${port}
+rosrun openrtm rtinject -p /tmp -m RTMROSDataBridge -n ${count} -c "${data}" ${port}
 
 # this script may not work in OpenRTM-Python-1.1.0
 # ModuleMgr.find_class in rtshell does not parse dataport.data_type
