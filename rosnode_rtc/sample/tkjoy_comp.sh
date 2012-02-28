46d3e2a5abe1803a784b6346f60dc2170e51737a
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-02-28 10:54:56 +0000

    add cut to remove space fro the rospack export ...

--- rosnode_rtc/sample/tkjoy_comp.sh
+++ rosnode_rtc/sample/tkjoy_comp.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -i
 
-export RTCTREE_NAMESERVERS=localhost
-export PATH=$PATH:`rospack find openrtm`/bin
-export PYTHONPATH=$PYTHONPATH:`rospack find openrtm`/src/openrtm
-export PYTHONPATH=$PYTHONPATH:`rospack find openrtm`/lib/python2.7/site-packages/OpenRTM_aist/RTM_IDL/
-
 roscd openrtm/build/OpenRTM-aist-Python-1.0.1/OpenRTM_aist/examples/TkJoyStick
-python TkJoyStickComp.py
+PYTHONPATH=$PYTHONPATH:`rospack export --lang=python --attrib=path openrtm | cut -d\  -f1` python TkJoyStickComp.py
