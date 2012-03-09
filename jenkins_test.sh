1faf8d90b1e74a7962ef57ed20c1758e6b270daf
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-03-09 02:31:34 +0000

    we assume .static does not exit warning

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -12,7 +12,7 @@ function test-grxui {
     local package=$1
     trap error ERR
     rosmake --status-rate=0 --test-only $package
-    (cd `rospack find $package`; rosrun rosdoc rosdoc $package 2>&1 | tee build/rosdoc.log; grep Errno build/rosdoc.log && exit 1)
+    (cd `rospack find $package`; rosrun rosdoc rosdoc $package 2>&1 | tee build/rosdoc.log; [ `grep WARNING build/rosdoc.log | wc -l` -gt 1 ] && exit 1) # we assume .static does not exist... warning
 
     if [ -n "$WORKSPACE" ]; then # only for jenkins to copy to results
 	rm -fr $WORKSPACE/$package-example
