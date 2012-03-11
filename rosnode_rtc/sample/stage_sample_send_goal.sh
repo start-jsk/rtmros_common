4b1644d53372acd19cb4114272137383c565ebb2
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-03-11 20:56:58 +0000

    more verbose for rosnode_rtc example

--- rosnode_rtc/sample/stage_sample_send_goal.sh
+++ rosnode_rtc/sample/stage_sample_send_goal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -i
 
+set -x
+
 pos_x=21.0
-pos_y=20    # 15,20,25
+pos_y=25    # 15,20,25
 port=move_base_simple_goal
 data="RTMROSDataBridge.geometry_msgs_PoseStamped(header=RTMROSDataBridge.std_msgs_Header(seq=0, stamp=RTC.Time(sec=0, nsec=0), frame_id='/map'), pose=RTMROSDataBridge.geometry_msgs_Pose(position=RTMROSDataBridge.geometry_msgs_Point(x=${pos_x}, y=${pos_y}, z=0.0), orientation=RTMROSDataBridge.geometry_msgs_Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)))"
 
