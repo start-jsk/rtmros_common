76645e646ca67244cb456fb5f52fd0386c6c5c04
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-03-27 00:25:23 +0000

    move compile_openhrp_model,compile_collada_robot to cmake/compile_robot_model.cmake

---
+++ hrpsys_ros_bridge/cmake/compile_robot_model.cmake
@@ -0,0 +1,59 @@
+##
+## define macros
+##
+macro(compile_openhrp_model wrlfile)
+  set(_workdir ${PROJECT_SOURCE_DIR}/models)
+  if("${ARGN}" STREQUAL "")
+    get_filename_component(_name ${wrlfile} NAME_WE)
+  else()
+    set(_name "${ARGN}")
+  endif()
+  set(_daefile "${_workdir}/${_name}.dae")
+  set(_xmlfile "${_workdir}/${_name}.xml")
+  string(TOLOWER ${_name} _name)
+  set(_yamlfile "${_workdir}/${_name}.yaml")
+  set(_lispfile "${_workdir}/${_name}.l")
+  if(EXISTS ${_yamlfile})
+    add_custom_command(OUTPUT ${_lispfile}
+      COMMAND rosrun euscollada collada2eus ${_daefile} ${_yamlfile} ${_lispfile}
+      DEPENDS ${_daefile})
+  else(EXISTS ${_yamlfile})
+    add_custom_command(OUTPUT ${_lispfile}
+      COMMAND rosrun euscollada collada2eus ${_daefile} ${_lispfile}
+      DEPENDS ${_daefile})
+  endif(EXISTS ${_yamlfile})
+  add_custom_command(OUTPUT ${_daefile}
+    COMMAND rosrun openhrp3 export-collada -i ${wrlfile} -o ${_daefile}
+    DEPENDS ${wrlfile})
+  add_custom_command(OUTPUT ${_xmlfile}
+    COMMAND rostest -t hrpsys _gen_project.launch INPUT:=${wrlfile} OUTPUT:=${_xmlfile}
+    DEPENDS ${wrlfile})
+  add_custom_target(${_name}_compile DEPENDS ${_lispfile} ${_xmlfile} ${_daefile})
+  list(APPEND compile_robots ${_name}_compile)
+endmacro(compile_openhrp_model)
+
+macro(compile_collada_robot daefile)
+  set(_workdir ${PROJECT_SOURCE_DIR}/models)
+  get_filename_component(_name ${daefile} NAME_WE)
+  set(_xmlfile "${_workdir}/${_name}.xml")
+  string(TOLOWER ${_name} _name)
+  set(_yamlfile "${_workdir}/${_name}.yaml")
+  set(_lispfile "${_workdir}/${_name}.l")
+
+  if(EXISTS ${_yamlfile})
+    add_custom_command(OUTPUT ${_lispfile}
+      COMMAND rosrun euscollada collada2eus ${daefile} ${_yamlfile} ${_lispfile}
+      DEPENDS ${daefile})
+  else(EXISTS ${_yamlfile})
+    add_custom_command(OUTPUT ${_lispfile}
+      COMMAND rosrun euscollada collada2eus ${daefile} ${_lispfile} || echo 'ok'
+      DEPENDS ${daefile})
+  endif(EXISTS ${_yamlfile})
+  add_custom_command(OUTPUT ${_xmlfile}
+    COMMAND rostest -t hrpsys _gen_project.launch INPUT:=${daefile} OUTPUT:=${_xmlfile}
+    DEPENDS ${daefile})
+  add_custom_target(${_name}_compile DEPENDS ${_lispfile} ${_xmlfile})
+  list(APPEND compile_robots ${_name}_compile)
+endmacro(compile_collada_robot daefile)
+
+
