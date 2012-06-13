7089a63996e9e401c6bc1ddc3c50fde0320f641b
nakaokat <nakaokat@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-06-13 02:50:05 +0000

    delete duplicated source in rtshell-setup.sh

--- openrtm/scripts/rtshell-setup.sh
+++ openrtm/scripts/rtshell-setup.sh
@@ -2,7 +2,6 @@
 
 export PATH=`rospack find openrtm`/bin:$PATH
 export PYTHONPATH=`rospack find openrtm`/lib/`python -c 'import sys; print "python%s"%sys.version[:3]'`/site-packages:$PYTHONPATH
-. `rospack find openrtm`/share/rtshell/bash_completion
 . `rospack find openrtm`/share/rtshell/shell_support
 
 export RTCTREE_NAMESERVERS=localhost
