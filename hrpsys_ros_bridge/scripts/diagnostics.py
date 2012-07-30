ac43d3c0281822dbdbba46a2f64a0916fe5dd251
kei.okada@gmail.com <kei.okada@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-07-30 05:54:50 +0000

    add diagnostics.py to publish /Mode/Operating Mode/{Servo On,Servo Off}

---
+++ hrpsys_ros_bridge/scripts/diagnostics.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('hrpsys_ros_bridge')
+import rospy
+import time
+from std_msgs.msg import String
+from diagnostic_msgs.msg import *
+from hrpsys_ros_bridge.msg import MotorStates
+
+def states_cb(msg):
+    diagnostic = DiagnosticArray()
+    diagnostic.header.stamp = msg.header.stamp
+    status = DiagnosticStatus(name = 'Operating Mode', level = DiagnosticStatus.OK, message = "Servo On")
+
+    for i in range(len(msg.name)) :
+        if ( msg.servo_alarm[i] > 0 ) :
+            status.level   = DiagnosticStatus.ERROR
+            status.message = "Servo Off"
+
+    diagnostic.status.append(status)
+    pub.publish(diagnostic)
+
+if __name__ == '__main__':
+    try:
+        global initial_flag, time_latest
+        rospy.init_node('hrpsys_diagnostics')
+        sub = rospy.Subscriber('motor_states', MotorStates, states_cb)
+        pub = rospy.Publisher('diagnostics', DiagnosticArray)
+
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
