0321692914b511e443d1d3afdbd3dd5a353212dc
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2012-11-26 02:04:16 +0000

    add emergence_stop.py

---
+++ hrpsys_ros_bridge/scripts/emergence_stop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import roslib
+roslib.load_manifest('hrpsys_ros_bridge')
+
+import rospy
+from sensor_msgs.msg import Joy
+
+import hrpsys
+from hrpsys.srv import *
+
+def joystick_callback(msg) :
+    if msg.buttons[3] == 1: # push start
+        rospy.loginfo("push start button")
+        servo = rospy.ServiceProxy("/RobotHardwareServiceROSBridge/servo", OpenHRP_RobotHardwareService_servo )
+        power = rospy.ServiceProxy("/RobotHardwareServiceROSBridge/power", OpenHRP_RobotHardwareService_power )
+        try:
+            servo(OpenHRP_RobotHardwareService_servoRequest("all",1));
+            time.sleep(1)
+            power(OpenHRP_RobotHardwareService_powerRequest("all",1))
+        except rospy.ServiceException, e:
+            rospy.logerr("Failed to put the hrpsys in serv off mode: service call failed with error: %s"%(e))
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('emergence_stop')
+        sub = rospy.Subscriber('/joy', Joy, joystick_callback);
+        r = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            r.sleep()
+    except rospy.ROSInterruptException: pass
+
+
