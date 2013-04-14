3dc19401c3db65421805b8cd68036cfc2cf86348
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-04-14 02:46:49 +0000

    print warning if openrtm/share/rtshell/shell_support is not found

--- openrtm/scripts/rtshell-setup.sh
+++ openrtm/scripts/rtshell-setup.sh
@@ -2,7 +2,11 @@
 
 export PATH=`rospack find openrtm`/bin:$PATH
 export PYTHONPATH=`rospack find openrtm`/lib/`python -c 'import sys; print "python%s"%sys.version[:3]'`/site-packages:$PYTHONPATH
-. `rospack find openrtm`/share/rtshell/shell_support
+if [ -f `rospack find openrtm`/share/rtshell/shell_support ]; then
+    . `rospack find openrtm`/share/rtshell/shell_support
+else
+    echo -e "Warning : Failed to load shell_support, try rosmake openrtm"
+fi
 
 export RTCTREE_NAMESERVERS=localhost
 #echo ";; set RTCTREE_NAMESERVERS=$RTCTREE_NAMESERVERS"
