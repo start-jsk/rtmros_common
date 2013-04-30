964807d017de2f4b7fc354664fb0191641564598
youhei@jsk.imi.i.u-tokyo.ac.jp <youhei@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-04-30 10:21:49 +0000

    update for converting gazebo world to EusLisp model

--- hrpsys_gazebo/scripts/parse_world.py
+++ hrpsys_gazebo/scripts/parse_world.py
@@ -1,9 +1,76 @@
 #!/usr/bin/env python
 
 from xml.dom import minidom
+import os
+import urllib
+import urlparse
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+
+def search_gazebo_dir (dirname, gpath = ['.']):
+    for pdir in gpath:
+        if os.access(pdir+'/'+dirname, os.F_OK):
+            return pdir+'/'+dirname
+
+def resolve_gazebo_path (fname):
+    if fname.find('model://') >= 0:
+        up = urlparse.urlparse(fname)
+        dname = up.netloc
+        try:
+            dirs = os.environ['GAZEBO_MODEL_PATH'].split(':')
+            dirs.append('/tmp')
+        except:
+            print ""
+            return fname
+        gp = search_gazebo_dir (dname, gpath = dirs)
+        if gp:
+            return gp
+        else:
+            print ""
+            return fname
+    elif fname.find('file://media') >= 0:
+        up = urlparse.urlparse(fname)
+        dname = up.netloc + up.path
+        try:
+            dirs = os.environ['GAZEBO_RESOURCE_PATH'].split(':')
+        except:
+            print ""
+            return fname
+        gp = search_gazebo_dir (dname, gpath = dirs)
+        if gp:
+            return gp
+        else:
+            print ""
+            return fname
+    elif fname.find('package://') >= 0:
+        return
+    return fname
+
+def check_model_url (model):
+    model = resolve_gazebo_path(model)
+    pos = model.find('model://')
+    if pos == -1:
+        return model
+    modelname = model[pos+8:] ## remove model://
+    sys.stderr.write(";; model = %s\n"%modelname)
+    tmpmodel = '/tmp/%s' % modelname
+    if os.access(tmpmodel, os.F_OK):
+        sys.stderr.write(";; at TMP: %s\n"%modelname)
+        return tmpmodel
+    fp = urllib.urlopen("http://gazebosim.org/models/%s/model.tar.gz" % modelname)
+    if fp.getcode() == 200:
+        f = open("/tmp/model.tar.gz", 'wb')
+        f.write(fp.read())
+        fp.close()
+        f.close()
+        os.system('tar xf /tmp/model.tar.gz -C /tmp')
+        if os.access(tmpmodel, os.F_OK):
+            sys.stderr.write(";; create at TMP: %s\n"%modelname)
+            return tmpmodel
+    return model
 
 def getText(nodelist):
     rc = []
@@ -31,8 +98,26 @@ if __name__ == '__main__':
         for inc in wrd[0].getElementsByTagName('include'):
             modelf = getText(inc.getElementsByTagName('uri')[0].childNodes)
             pose = getText(inc.getElementsByTagName('pose')[0].childNodes)
+            modelf =  check_model_url(modelf)
+
             print "(list (list :model \"%s\")" % modelf
             if pose != '':
                 print "      (list :pose \"%s\")" % pose
             print ")"
+        for mdl in wrd[0].getElementsByTagName('model'):
+            nm = mdl.getAttribute('name')
+            if nm:
+                dirname = '/tmp/%s' % nm
+                if not os.access(dirname, os.F_OK):
+                    os.makedirs(dirname)
+                f = open('%s/model.sdf' % dirname, 'wb')
+                f.write('<?xml version="1.0"?>\n<sdf version="1.3">\n')
+                f.write(mdl.toxml())
+                f.write('</sdf>')
+                f.close()
+                print '(list (list :model \"%s\")' % dirname
+                pose = getText(mdl.getElementsByTagName('pose')[0].childNodes)
+                if pose:
+                    print '      (list :pose  \"%s\")' % pose
+                print ')'
         print ")"
