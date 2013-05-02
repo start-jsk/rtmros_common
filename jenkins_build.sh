4b385d653fb69213fd88c76050073caed0968d11
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-05-02 00:58:00 +0000

    fix for new jsk.rosbuild

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
 
-wget https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/jsk.rosbuild -O /tmp/jsk.rosbuild
+
+function usage {
+    echo >&2 "usage: $0"
+    echo >&2 "          [-h|--help] print this message"
+    echo >&2 "          [-t|--target] set compile package"
+    exit 0
+}
+
+
+# command line parse
+OPT=`getopt -o t:h -l help,target: -- $*`
+if [ $? != 0 ]; then
+    usage
+fi
+
+eval set -- $OPT
+while [ -n "$1" ] ; do
+    case $1 in
+	-h|--help) usage ;;
+	-t|--target) TARGET=$2; shift 2; break;;
+	--) shift; break;;
+	*) echo "Unknown option($1)"; usage;;
+    esac
+done
+
+wget 'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosbuild?format=raw' -O /tmp/jsk.rosbuild
 . /tmp/jsk.rosbuild -e $@
 
 function install-rtm-ros-robotics {
-    install-pkg http://jsk-ros-pkg.svn.sourceforge.net/viewvc/jsk-ros-pkg/trunk/jsk.rosinstall http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall
+    install-pkg  'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosinstall?format=raw' http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall
 }
+
 function compile-rtm-ros-robotics {
     compile-pkg euscollada rtmros_common openhrp3 hrpsys_ros_bridge mrobot_ros_bridge RS003 openrtm_ros_bridge
 }
-function test-jsk-ros-pkig {
-    test-pkg openrtm_ros_bridge openhrp3 hrpsys hrpsys_ros_bridge
-e_pr2
-}
 
-setup-ros
-apt-get-ros-package
-install-rtm-ros-robotics
-compile-rtm-ros-robotics
+#setup-ros
+#apt-get-ros-package
+#install-rtm-ros-robotics
+if ( [ "$TARGET" = "" ] ) then
+    echo "compile-rtm-ros-robotics"
+    compile-rtm-ros-robotics
+else
+    echo "compile-pkg $TARGET"
+    compile-pkg $TARGET
+fi
 
