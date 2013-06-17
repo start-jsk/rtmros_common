240b0d5d6dfff26005c8bade78575870be2085ea
nakaokat@gmail.com <nakaokat@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-06-17 11:06:32 +0000

    modify viewer script to wait launched nodes

--- hrpsys_gazebo/scripts/start_viewer.sh
+++ hrpsys_gazebo/scripts/start_viewer.sh
@@ -2,7 +2,14 @@
 
 rosparam set /use_sim_time false
 
+#roslaunch hrpsys_gazebo screenpoint_resize_view.launch COMPRESS_TYPE:=theora &
 roslaunch hrpsys_gazebo screenpoint_resize_view.launch &
+PID=$!
 
-sleep 2
+sleep 15
 rosparam set /use_sim_time true
+
+trap "kill -SIGINT $PID; wait $PID" 1 2 3 15
+
+wait $PID
+
