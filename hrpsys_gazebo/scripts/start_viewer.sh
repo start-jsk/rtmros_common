3d0edf3af44e5b3fe1d8781d9711d1141fc1e485
nakaokat@gmail.com <nakaokat@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-06-18 01:57:25 +0000

    send SIGINT to all image_views when pressed ^C

--- hrpsys_gazebo/scripts/start_viewer.sh
+++ hrpsys_gazebo/scripts/start_viewer.sh
@@ -7,12 +7,16 @@ roslaunch hrpsys_gazebo screenpoint_resize_view.launch &
 PID=$!
 
 rosrun image_view image_view image:=/head_snap/image_rect _image_transport:=compressed &
+PID="$PID $!"
 rosrun image_view image_view image:=/lhand_snap/image_rect _image_transport:=compressed &
+PID="$PID $!"
 rosrun image_view image_view image:=/rhand_snap/image_rect _image_transport:=compressed &
+PID="$PID $!"
 
 sleep 15
 rosparam set /use_sim_time true
 
-trap "kill -SIGINT $PID; wait $PID" 1 2 3 15
+trap "kill -SIGINT $PID; wait" 1 2 3 15
 
-wait $PID
+wait
+
