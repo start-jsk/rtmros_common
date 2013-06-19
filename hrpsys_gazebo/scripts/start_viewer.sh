a13e243db327fb56ec6cadffc75b780f0a9ea35a
murase@jsk.imi.i.u-tokyo.ac.jp <murase@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-06-19 06:45:36 +0000

    add rate argument to start_viewer.sh

--- hrpsys_gazebo/scripts/start_viewer.sh
+++ hrpsys_gazebo/scripts/start_viewer.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+RATE=0.25
+
+if [ "$1" != "" ]; then
+    RATE=$1
+fi
+
+echo "RATE = ${RATE}"
+
 rosparam set /use_sim_time false
 
 #roslaunch hrpsys_gazebo screenpoint_resize_view.launch COMPRESS_TYPE:=theora &
-roslaunch hrpsys_gazebo screenpoint_resize_view.launch &
+roslaunch hrpsys_gazebo screenpoint_resize_view.launch RESIZE_RATE:=${RATE}&
 PID=$!
 
 rosrun image_view image_view image:=/head_snap/image_rect _image_transport:=compressed &
