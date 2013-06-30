a6f26cbfac3ed0cf67adb07689290dea0c58cbe0
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-06-30 15:25:11 +0000

    add RTC service interface for seq_svc, sh_svc, fk_svc

--- hrpsys/scripts/hrpsys.py
+++ hrpsys/scripts/hrpsys.py
@@ -10,6 +10,83 @@ from OpenHRP import *
 import socket
 import time
 
+# copy from transformations.py, Christoph Gohlke, The Regents of the University of California
+
+import numpy
+# map axes strings to/from tuples of inner axis, parity, repetition, frame
+_AXES2TUPLE = {
+    'sxyz': (0, 0, 0, 0), 'sxyx': (0, 0, 1, 0), 'sxzy': (0, 1, 0, 0),
+    'sxzx': (0, 1, 1, 0), 'syzx': (1, 0, 0, 0), 'syzy': (1, 0, 1, 0),
+    'syxz': (1, 1, 0, 0), 'syxy': (1, 1, 1, 0), 'szxy': (2, 0, 0, 0),
+    'szxz': (2, 0, 1, 0), 'szyx': (2, 1, 0, 0), 'szyz': (2, 1, 1, 0),
+    'rzyx': (0, 0, 0, 1), 'rxyx': (0, 0, 1, 1), 'ryzx': (0, 1, 0, 1),
+    'rxzx': (0, 1, 1, 1), 'rxzy': (1, 0, 0, 1), 'ryzy': (1, 0, 1, 1),
+    'rzxy': (1, 1, 0, 1), 'ryxy': (1, 1, 1, 1), 'ryxz': (2, 0, 0, 1),
+    'rzxz': (2, 0, 1, 1), 'rxyz': (2, 1, 0, 1), 'rzyz': (2, 1, 1, 1)}
+
+# axis sequences for Euler angles
+_NEXT_AXIS = [1, 2, 0, 1]
+
+# epsilon for testing whether a number is close to zero
+_EPS = numpy.finfo(float).eps * 4.0
+
+def euler_from_matrix(matrix, axes='sxyz'):
+    """Return Euler angles from rotation matrix for specified axis sequence.
+
+    axes : One of 24 axis sequences as string or encoded tuple
+
+    Note that many Euler angle triplets can describe one matrix.
+
+    >>> R0 = euler_matrix(1, 2, 3, 'syxz')
+    >>> al, be, ga = euler_from_matrix(R0, 'syxz')
+    >>> R1 = euler_matrix(al, be, ga, 'syxz')
+    >>> numpy.allclose(R0, R1)
+    True
+    >>> angles = (4.0*math.pi) * (numpy.random.random(3) - 0.5)
+    >>> for axes in _AXES2TUPLE.keys():
+    ...    R0 = euler_matrix(axes=axes, *angles)
+    ...    R1 = euler_matrix(axes=axes, *euler_from_matrix(R0, axes))
+    ...    if not numpy.allclose(R0, R1): print axes, "failed"
+
+    """
+    try:
+        firstaxis, parity, repetition, frame = _AXES2TUPLE[axes.lower()]
+    except (AttributeError, KeyError):
+        _ = _TUPLE2AXES[axes]
+        firstaxis, parity, repetition, frame = axes
+
+    i = firstaxis
+    j = _NEXT_AXIS[i+parity]
+    k = _NEXT_AXIS[i-parity+1]
+
+    M = numpy.array(matrix, dtype=numpy.float64, copy=False)[:3, :3]
+    if repetition:
+        sy = math.sqrt(M[i, j]*M[i, j] + M[i, k]*M[i, k])
+        if sy > _EPS:
+            ax = math.atan2( M[i, j],  M[i, k])
+            ay = math.atan2( sy,       M[i, i])
+            az = math.atan2( M[j, i], -M[k, i])
+        else:
+            ax = math.atan2(-M[j, k],  M[j, j])
+            ay = math.atan2( sy,       M[i, i])
+            az = 0.0
+    else:
+        cy = math.sqrt(M[i, i]*M[i, i] + M[j, i]*M[j, i])
+        if cy > _EPS:
+            ax = math.atan2( M[k, j],  M[k, k])
+            ay = math.atan2(-M[k, i],  cy)
+            az = math.atan2( M[j, i],  M[i, i])
+        else:
+            ax = math.atan2(-M[j, k],  M[j, j])
+            ay = math.atan2(-M[k, i],  cy)
+            az = 0.0
+
+    if parity:
+        ax, ay, az = -ax, -ay, -az
+    if frame:
+        ax, az = az, ax
+    return ax, ay, az
+
 # class for configure hrpsys RTCs and ports
 #   In order to specify robot-dependent code, please inherit this HrpsysConfigurator
 class HrpsysConfigurator:
@@ -254,6 +331,62 @@ class HrpsysConfigurator:
         while self.findModelLoader() == None: # seq uses modelloader
             print self.configurator_name, "wait for ModelLoader"
             time.sleep(3);
+
+    ##
+    ## service interface for RTC component
+    ##
+    def goActual(self):
+        self.sh_svc.goActual()
+
+    def setJointAngle(self, jname, angle, tm):
+        radangle = angle/180.0*math.pi
+        return self.seq_svc.setJointAngle(jname, radangle, tm)
+
+    def setJointAngles(self, pose, tm):
+        angles = []
+        for item in pose:
+            angles.append(item/180.0*math.pi)
+        return self.seq_svc.setJointAngles(angles, tm)
+
+    def setJointAnglesOfGroup(self, gname, pose, tm, wait=True):
+        angles = [x/180.0*math.pi for x in pose]
+        ret = self.seq_svc.setJointAnglesOfGroup(gname, angles, tm)
+        if wait:
+            self.waitInterpolationOfGroup(gname)
+        return ret
+
+    def waitInterpolation(self):
+        self.seq_svc.waitInterpolation()
+
+    def waitInterpolationOfGroup(self, gname):
+        self.seq_svc.waitInterpolationOfGroup(gname)
+
+    def getJointAngles(self):
+        return [x*180.0/math.pi for x in self.sh_svc.getCommand().jointRefs]
+
+    def getReferencePose(self,lname):
+        pose = self.fk_svc.getReferencePose(lname)
+        if not pose[0] :
+            raise RuntimeError("Could not find reference : " + lname)
+        return pose[1].data
+
+    def getReferencePosition(self,lname):
+        pose = self.getReferencePose(lname)
+        return [pose[3],pose[7],pose[11]]
+
+    def getReferenceRotation(self,lname):
+        pose = self.getReferencePose(lname)
+        return [pose[0:3],pose[4:7],pose[8:11]]
+
+    def getReferenceRPY(self,lname):
+        return euler_from_matrix(self.getReferenceRotation(lname),'sxyz')
+
+    def setTargetPose(self, gname, pos, rpy, tm) :
+        return self.seq_svc.setTargetPose(gname, pos, rpy, tm)
+
+    ###
+    ### initialize
+    ###
 
     def init(self, robotname="Robot", url=""):
         self.url = url
