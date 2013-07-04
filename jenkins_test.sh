efbdf33b60ec60b63a59809f479301c5174830bb
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-04 05:34:07 +0000

    update jeknins_test to use jsk.rosbuild

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -8,34 +8,15 @@ function error {
 rm -fr $ROS_HOME/test_results/
 trap error ERR
 
-function test-grxui {
-    local package=$1
-    trap error ERR
-    (cd `rospack find $package`; make test || (sudo /etc/init.d/omniorb4-nameserver stop; rosrun openhrp openhrp3-shutdown-servers; make test) )
-    (cd `rospack find $package`; rosrun rosdoc rosdoc $package 2>&1 | tee build/rosdoc.log; [ `grep "WARNING: cannot copy" build/rosdoc.log | wc -l` -gt 0 ] && exit 1 || true) # we assume .static does not exist... warning
+wget 'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosbuild?format=raw' -O /tmp/jsk.rosbuild.$$
+/tmp/jsk.rosbuild.$$ $1 -e
 
-    (cd `rospack find $package`; copy build/index.rst .; copy build/conf.py .;svn add --non-interactive --username rtmrosrobotics.testing@gmail.com --password XC6HC3Jy2FG3 index.rst conf.py; svn commit --non-interactive --username rtmrosrobotics.testing@gmail.com --password XC6HC3Jy2FG3 -m "update index.rst,conf.py by Jenkins" index.rst conf.py)
-    if [ -n "$WORKSPACE" ]; then # only for jenkins to copy to results
-	rm -fr $WORKSPACE/$package-example
-	cp -r `rospack find $package`/doc/$package/html $WORKSPACE/$package-example
-    fi
-}
+install-pkg 'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosinstall?format=raw' http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall
 
-# do test
-set -x
-rm -fr `rospack find openhrp3`/workspace/
-sudo /etc/init.d/omniorb4-nameserver stop
-(rosrun openhrp3 openhrp-shutdown-servers; exit 0)
-test-grxui openhrp3
-test-grxui hrpsys
+compile-pkg hrpsys_tutorials hrpsys_ros_bridge_tutorials hironx_ros_bridge
 
-# resize eclipse window size
-#sed -i 's/height="[0-9]*" width="[0-9]*"/height="723" width="506"/' `rospack find openhrp3`/workspace/.metadata/.plugins/org.eclipse.ui.workbench/workbench.xml
-#head `rospack find openhrp3`/workspace/.metadata/.plugins/org.eclipse.ui.workbench/workbench.xml
-# do test
-test-grxui hrpsys_ros_bridge
+test-pkg hrpsys_tutorials hrpsys_ros_bridge_tutorials hironx_ros_bridge
 
-# done
-rosrun rosunit clean_junit_xml.py
-
+#
+(which rosrun && rosrun rosunit clean_junit_xml.py; echo "done")     # check error
 
