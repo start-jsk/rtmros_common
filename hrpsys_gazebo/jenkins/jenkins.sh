25fe9d431cf86c6e9ecf9d5dbf50c8a4a8296175
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-11 06:29:24 +0000

    add : install drcsim and merge ROS_PACKAGE_PATH

--- hrpsys_gazebo/jenkins/jenkins.sh
+++ hrpsys_gazebo/jenkins/jenkins.sh
@@ -5,6 +5,16 @@ wget 'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosbuild?format=raw' -
 
 function install-hrpsys-atlas {
     install-pkg 'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosinstall?format=raw' http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall
+    # install drcsim
+    sudo sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu precise main" > /etc/apt/sources.list.d/drc-latest.list'
+    wget http://packages.osrfoundation.org/drc.key -O - | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install gazebo drcsim
+    # merge ROS_PACKAGE_PATh
+    ROS_PACKAGE_PATH_ORG=$ROS_PACKAGE_PATH
+    source /usr/share/drcsim/setup.sh
+    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH_ORG:$ROS_PACKAGE_PATH
+    export ROS_PACKAGE_PATH=`echo $(echo $ROS_PACKAGE_PATH | sed -e "s/:/\n/g" | awk '!($0 in A) && A[$0] = 1' | grep -v "opt/ros"; echo $ROS_PACKAGE_PATH | sed -e "s/:/\n/g" | awk '!($0 in A) && A[$0] = 1' | grep "opt/ros") | sed -e "s/ /:/g"`
 }
 
 function compile-hrpsys-atlas {
