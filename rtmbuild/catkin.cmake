b33b5b557fa4728f3d6ddd30f2c27e7d071f832b
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-22 12:17:35 +0000

    catkinize package : add catkin.cmake and package.xml

---
+++ rtmbuild/catkin.cmake
@@ -0,0 +1,77 @@
+# http://ros.org/doc/groovy/api/catkin/html/user_guide/supposed.html
+cmake_minimum_required(VERSION 2.8.3)
+project(rtmbuild)
+# Load catkin and all dependencies required for this package
+# TODO: remove all from COMPONENTS that are not catkin packages.
+find_package(catkin REQUIRED COMPONENTS std_msgs)
+find_package(PkgConfig)
+pkg_check_modules(OMNIORB REQUIRED omniORB4)
+if (!OMNIORB_FOUND)
+  message(FATAL_ERROR  "OmniORB4 is not found")
+else()
+endif()
+
+# include_directories(include ${Boost_INCLUDE_DIR} ${catkin_INCLUDE_DIRS})
+# CATKIN_MIGRATION: removed during catkin migration
+# cmake_minimum_required(VERSION 2.4.6)
+
+# CATKIN_MIGRATION: removed during catkin migration
+# include($ENV{ROS_ROOT}/core/rosbuild/rosbuild.cmake)
+
+# Set the build type.  Options are:
+#  Coverage       : w/ debug symbols, w/o optimization, w/ code-coverage
+#  Debug          : w/ debug symbols, w/o optimization
+#  Release        : w/o debug symbols, w/ optimization
+#  RelWithDebInfo : w/ debug symbols, w/ optimization
+#  MinSizeRel     : w/o debug symbols, w/ optimization, stripped binaries
+#set(ROS_BUILD_TYPE RelWithDebInfo)
+
+
+# CATKIN_MIGRATION: removed during catkin migration
+# rosbuild_init()
+
+#set the default path for built executables to the "bin" directory
+set(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin)
+#set the default path for built libraries to the "lib" directory
+set(LIBRARY_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/lib)
+
+#uncomment if you have defined messages
+#add_message_files(
+  # TODO: List your msg files here
+#)
+#uncomment if you have defined services
+#add_service_files(
+#  FILES
+  # TODO: List your msg files here
+#)
+
+#common commands for building c++ executables and libraries
+#add_library(${PROJECT_NAME} src/example.cpp)
+#target_link_libraries(${PROJECT_NAME} another_library)
+#
+# CATKIN_MIGRATION: removed during catkin migration
+# rosbuild_add_boost_directories()
+#find_package(Boost REQUIRED COMPONENTS thread)
+#include_directories(${Boost_INCLUDE_DIRS})
+#target_link_libraries(${PROJECT_NAME} ${Boost_LIBRARIES})
+#add_executable(example examples/example.cpp)
+#target_link_libraries(example ${PROJECT_NAME})
+## Generate added messages and services with any dependencies listed here
+#generate_messages(
+  #TODO DEPENDENCIES geometry_msgs std_msgs
+#)
+# TODO: fill in what other packages will need to use this package
+## LIBRARIES: libraries you create in this project that dependent projects also need
+## CATKIN_DEPENDS: catkin_packages dependent projects also need
+## DEPENDS: system dependencies of this project that dependent projects also need
+catkin_package(
+    DEPENDS # TODO
+    CATKIN-DEPENDS std_msgs
+    INCLUDE_DIRS # TODO include
+    LIBRARIES # TODO
+)
+
+# install cmake files for catkin (except some files only used at build time of catkin)
+install(DIRECTORY cmake
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  PATTERN ".svn" EXCLUDE)
