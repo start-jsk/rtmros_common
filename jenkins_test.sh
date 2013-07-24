11b60220af331e9a7b76dcd94fcd420b425abb77
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-24 16:34:33 +0000

    add openrtm_ros_bridge to test

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -15,7 +15,7 @@ install-pkg 'http://svn.code.sf.net/p/jsk-ros-pkg/code/trunk/jsk.rosinstall?form
 
 rosdep update || ( sleep 120; rosdep update )
 
-PKGS='openrtm euscollada openhrp3 hrpsys_tutorials hironx_ros_bridge'
+PKGS='openrtm openrtm_ros_bridge euscollada openhrp3 hrpsys_tutorials hironx_ros_bridge'
 
 compile-pkg $PKGS
 
