995edb547362c8fa14375ca5c1c00d6274914e76
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-24 16:32:17 +0000

    add test code in python, add HAVE_DISPLAY arg for test environment

---
+++ openrtm_ros_bridge/samples/test_myservice_rosbridge.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+PKG = 'openrtm_ros_bridge'
+import roslib; roslib.load_manifest(PKG)
+import rospy
+
+import unittest
+import openrtm_ros_bridge.srv
+#from openrtm_ros_bridge.srv import *
+
+class TestMyServiceRosBridge(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        rospy.init_node('test_myservice_rosbridge')
+
+    def testEcho(self):
+        rospy.wait_for_service("/bridge/echo")
+        echo = rospy.ServiceProxy("/bridge/echo", openrtm_ros_bridge.srv.SimpleService_MyService_echo)
+        req = openrtm_ros_bridge.srv.SimpleService_MyService_echo
+        msg = "this is test data 123"
+        res = echo(msg)
+        self.assertTrue(res.operation_return == msg, "SimpleService.echo returns incorrect string")
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun(PKG, 'test_myservice_rosbridge', TestMyServiceRosBridge)
