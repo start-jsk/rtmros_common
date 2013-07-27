466716332aaa4df76714feecfdcefe32cfc75c90
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-27 17:18:08 +0000

    rtm-config is in openrtm_aist, due to split openrtm into openrtm_aist, openrtm_aist_python, rtshell, rtctree and rtsprofile, Issue 137

--- openrtm_ros_bridge/cmake/servicebridge.cmake
+++ openrtm_ros_bridge/cmake/servicebridge.cmake
@@ -11,7 +11,7 @@ macro(rtmbuild_genbridge_init)
     file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/srv)
   endif(NOT _idllist)
 
-  execute_process(COMMAND ${_openrtm_pkg_dir}/bin/rtm-config --cflags OUTPUT_VARIABLE _rtm_include_dir OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND ${_openrtm_aist_pkg_dir}/bin/rtm-config --cflags OUTPUT_VARIABLE _rtm_include_dir OUTPUT_STRIP_TRAILING_WHITESPACE)
   execute_process(COMMAND sh -c "echo ${_rtm_include_dir} | sed 's/^-[^I]\\S*//g' | sed 's/\ -[^I]\\S*//g' | sed 's/-I//g'" OUTPUT_VARIABLE _rtm_include_dir OUTPUT_STRIP_TRAILING_WHITESPACE)
   set(_include_dirs "${_rtm_include_dir} ${_openhrp3_pkg_dir}/share/OpenHRP-3.1/idl")
 
