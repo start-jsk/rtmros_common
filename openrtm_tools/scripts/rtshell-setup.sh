bb5d5a9b87b5d52acaf0d098c04c6326598137c1
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-29 14:21:40 +0000

    fix install rtshell under /bin, /src, see Issue 146

--- openrtm_tools/scripts/rtshell-setup.sh
+++ openrtm_tools/scripts/rtshell-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export PATH=`rospack find openrtm_aist`/bin:$PATH
+export PATH=`rospack find rtshell`/bin:$PATH
 export PYTHONPATH=`rospack find openrtm_aist_python`/lib/`python -c 'import sys; print "python%s"%sys.version[:3]'`/site-packages:$PYTHONPATH
-if [ -f `rospack find rtshell`/src/share/rtshell/shell_support ]; then
-    (cd `rospack find rtshell`/src/share/; . ./rtshell/shell_support)
+if [ -f `rospack find rtshell`/share/rtshell/shell_support ]; then
+    (cd `rospack find rtshell`/share/; . ./rtshell/shell_support)
 else
     echo -e "Warning : Failed to load shell_support, try rosmake openrtm"
 fi
