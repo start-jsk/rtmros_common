0a7e60cfbf1bfc017c2b4a59c60bec4e789441e6
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-29 17:13:10 +0000

    delete rtmros_common/hironx_ros_bridge_old/ rtmros_common/openrtm rtmros_common/tvmet/ rtmros_common/rosdep.yaml rtmros_common/CMakeLists.txt rtmros_common/Makefile, Issue 137

--- hironx_ros_bridge_old/gui/gui.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "`rospack find hironx_ros_bridge`/gui/.robothost"
-cat `rospack find hironx_ros_bridge`/gui/.robothost
-rosrun hrpsys hrpsyspy `rospack find hironx_ros_bridge`/gui/gui.py
