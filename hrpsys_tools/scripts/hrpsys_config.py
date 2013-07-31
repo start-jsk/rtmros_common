dd9b498dede176a033b45e0f618f664eeff7c382
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-07-31 02:45:41 +0000

    create dummy hrpsys_config for ros users : due to hrpsys_config.py move to hrpsys-base, see https://code.google.com/p/hrpsys-base/source/browse/trunk/python/hrpsys_config.py

---
+++ hrpsys_tools/scripts/hrpsys_config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest("hrpsys")
+
+import sys
+from hrpsys_config import HrpsysConfigurator
+
+# copy from hrpsys/lib/python2.7/dist-packages/hrpsys_config.py
+if __name__ == '__main__':
+    hcf = HrpsysConfigurator()
+    if len(sys.argv) > 2 :
+        hcf.init(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) > 1 :
+        hcf.init(sys.argv[1])
+    else :
+        hcf.init()
+
