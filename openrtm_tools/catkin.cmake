22559b7cf121057c2fa7422fe8e023f5036da6a8
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-08-01 09:19:38 +0000

    catkinze package, add rtshell to build_depend

--- openrtm_tools/catkin.cmake
+++ openrtm_tools/catkin.cmake
@@ -4,7 +4,7 @@ project(openrtm_tools)
 ## Find catkin macros and libraries
 ## if COMPONENTS list like find_package(catkin REQUIRED COMPONENTS xyz)
 ## is used, also find other catkin packages
-find_package(catkin REQUIRED COMPONENTS openrtm_aist openrtm_aist_python)
+find_package(catkin REQUIRED COMPONENTS openrtm_aist openrtm_aist_python rtshell)
 
 ## System dependencies are found with CMake's conventions
 # find_package(Boost REQUIRED COMPONENTS system)
