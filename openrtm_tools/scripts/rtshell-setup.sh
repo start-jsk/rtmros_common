dfa310eb395a41d3ed18780ff9be8db33d4c9e0d
nakaokat@gmail.com <nakaokat@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-08-26 05:06:44 +0000

    switch completion settings of rtmlaunch by CATKIN_SHELL (for zsh users)

--- openrtm_tools/scripts/rtshell-setup.sh
+++ openrtm_tools/scripts/rtshell-setup.sh
@@ -11,5 +11,12 @@ else
 fi
 
 export RTCTREE_NAMESERVERS=localhost:5005
-complete -F "_roscomplete_launch" -o filenames "rtmlaunch"
+
+# these completion settings are copied from rosbash
+case "$CATKIN_SHELL" in
+    "bash" )
+        complete -F "_roscomplete_launch" -o filenames "rtmlaunch" ;;
+    "zsh" )
+        compctl -/g '*.(launch|test)' -x 'p[1]' -K "_roscomplete" -tx - 'p[2]' -K _roscomplete_launchfile -- + -x 'S[--]' -k "(--files --args --nodes --find-node --child --local --screen --server_uri --run_id --wait --port --core --pid --dump-params)" -- "rtmlaunch"
+esac
 #echo ";; set RTCTREE_NAMESERVERS=$RTCTREE_NAMESERVERS"
