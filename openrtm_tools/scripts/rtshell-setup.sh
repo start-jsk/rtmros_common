9bde9aed94cb7adb47ff0aaa46a3b1c9f771d0a8
youhei@jsk.imi.i.u-tokyo.ac.jp <youhei@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-08-27 08:29:43 +0000

    fix not using sub shell, [#179]

--- openrtm_tools/scripts/rtshell-setup.sh
+++ openrtm_tools/scripts/rtshell-setup.sh
@@ -5,7 +5,7 @@ export PATH=`rospack find rtshell`/bin:$PATH
 _PYTHONVER=`python -c 'import sys; print "python%s"%sys.version[:3]'`
 export PYTHONPATH=`rospack find openrtm_aist_python`/lib/${_PYTHONVER}/site-packages:`rospack find rtshell`/lib/${_PYTHONVER}/site-packages:`rospack find rtctree`/lib/${_PYTHONVER}/site-packages:`rospack find rtsprofile`/lib/${_PYTHONVER}/site-packages:$PYTHONPATH
 if [ -f `rospack find rtshell`/share/rtshell/shell_support ]; then
-    (cd `rospack find rtshell`/share/; . ./rtshell/shell_support)
+    source `rospack find rtshell`/share/rtshell/shell_support
 else
     echo -e "Warning : Failed to load shell_support, try rosmake openrtm"
 fi
