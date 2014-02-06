1ed60e7db1178e95c3435bc67743c16ac38c0da0
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-02-06 02:12:12 +0000

    set custom cmake file under CFG_EXTRAS, so that other package is abel to use macros defined in the cmake file

--- hrpsys_ros_bridge/catkin.cmake
+++ hrpsys_ros_bridge/catkin.cmake
@@ -51,6 +51,7 @@ catkin_package(
     CATKIN-DEPENDS rtmbuild roscpp sensor_msgs robot_state_publisher actionlib control_msgs tf camera_info_manager image_transport dynamic_reconfigure # pr2_controllers_msgs robot_monitor
     INCLUDE_DIRS # TODO include
     LIBRARIES # TODO
+    CFG_EXTRAS compile_robot_model.cmake
 )
 
 # generate idl
