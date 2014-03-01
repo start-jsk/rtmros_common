1a7308065039e83b8aad82991996712c28915c74
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-03-02 03:53:47 +0900

    add interactive mode, invoke with ipython ... or -i option

--- hrpsys_tools/scripts/hrpsys_tools_config.py
+++ hrpsys_tools/scripts/hrpsys_tools_config.py
@@ -3,15 +3,37 @@ import roslib; roslib.load_manifest("hrpsys")
 import OpenRTM_aist.RTM_IDL # for catkin
 import sys
 
-import hrpsys
+from hrpsys import rtm
 from hrpsys.hrpsys_config import *
 import OpenHRP
-#from hrpsys.hrpsys_config import HrpsysConfigurator
 
 # copy from hrpsys/lib/python2.7/dist-packages/hrpsys_config.py
+import argparse, code
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="hrpsys command line interpreters, try `./hrpsys_tools_config.py--host xxxx --port xxxx  -i` or `ipython ./hrpsys_tools_config.py -- --host xxxx --port xxxx` for robot debugging, we recommend to use ipython")
+    parser.add_argument('--host', help='corba name server hostname')
+    parser.add_argument('--port', help='corba name server port number')
+    parser.add_argument('-i', help='interactive mode',  action='store_true')
+    args, unknown = parser.parse_known_args()
+
+    if args.i: # interactive
+        sys.argv.remove('-i')
+    if args.host:
+        rtm.nshost = args.host; sys.argv = [sys.argv[0]] + sys.argv[3:]
+    if args.port:
+        rtm.nsport = args.port; sys.argv = [sys.argv[0]] + sys.argv[3:]
+
+    # ipython ./hrpsys_tools_config.py -- --port 2809  -i
     hcf = HrpsysConfigurator()
-    if len(sys.argv) > 2 :
+    if args.i or '__IPYTHON__' in vars(__builtins__):
+        hcf.findComps()
+        print >> sys.stderr, "[hrpsys.py] #\n[hrpsys.py] # use `hcf` as robot interface, for example hcf.getJointAngles()\n[hrpsys.py] #"
+        while len(sys.argv) >= 2:
+            print >> sys.stderr, ">>", sys.argv[1]
+            exec(sys.argv[1])
+            sys.argv = [sys.argv[0]] + sys.argv[2:]
+        code.interact(local=locals())
+    elif len(sys.argv) > 2 :
         hcf.init(sys.argv[1], sys.argv[2])
     elif len(sys.argv) > 1 :
         hcf.init(sys.argv[1])
