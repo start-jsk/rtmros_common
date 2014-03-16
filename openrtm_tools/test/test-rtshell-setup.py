e3bbf0c9d1168a9fe630224fa0c522811d63c637
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-03-16 16:41:07 +0900

    display output when test fails

--- openrtm_tools/test/test-rtshell-setup.py
+++ openrtm_tools/test/test-rtshell-setup.py
@@ -4,6 +4,7 @@ PKG = 'openrtm_tools'
 NAME = 'test-rthsell-setup'
 
 import unittest, os, sys
+import subprocess
 from subprocess import call, check_output, Popen, PIPE, STDOUT
 
 class TestOpenrtmTools(unittest.TestCase):
@@ -12,7 +13,10 @@ class TestOpenrtmTools(unittest.TestCase):
         pkg_path = check_output("rospack find openrtm_tools", shell=True).rstrip()
         script = os.path.join(pkg_path, "scripts/rtshell-setup.sh")
         self.assertTrue(os.path.exists(script))
-        print check_output("bash -c 'source %s; RTCTREE_NAMESERVERS=localhost:2809 rtls'"%(script), shell=True)
+        try:
+            check_output("bash -c 'source %s; RTCTREE_NAMESERVERS=localhost:2809 rtls'"%(script), shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError, (e):
+            self.assertTrue(False, 'subprocess.CalledProcessError: cmd:%s returncode:%s output:%s' % (e.cmd, e.returncode, e.output))
         self.assertTrue(True) # ok rtls works
 
 #unittest.main()
