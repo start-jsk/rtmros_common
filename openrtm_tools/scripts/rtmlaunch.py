59eb442f6b3c5c0d5ff8bf4d3d060946804a038e
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-03-18 18:06:10 +0900

    (rtmlaunch.py) trap SIGINT and exits with 0

--- openrtm_tools/scripts/rtmlaunch.py
+++ openrtm_tools/scripts/rtmlaunch.py
@@ -214,7 +214,14 @@ def get_flag_from_argv(arg):
         if arg in a: # If "USE_WALKING" is in argv
             return True if 'true' in a.split("=")[1] else False
 
+import signal
+def signal_handler(signum, frame):
+    sigdict = dict((k, v) for v, k in signal.__dict__.iteritems() if v.startswith('SIG'))
+    print >>sys.stderr, "\033[33m[rtmlaunch] Catch signal %r, exitting...\033[0m"%(sigdict[signum])
+    sys.exit(0)
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     main()
 
 
