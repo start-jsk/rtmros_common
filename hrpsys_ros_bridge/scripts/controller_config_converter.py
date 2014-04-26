79df1b72d54cf1cd300b0027cde5420fd7281ca7
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2014-04-26 21:39:53 +0900

    update generate config.yaml

--- hrpsys_ros_bridge/scripts/controller_config_converter.py
+++ hrpsys_ros_bridge/scripts/controller_config_converter.py
@@ -3,17 +3,32 @@ import yaml
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'usage : %s input.yaml [ output.yaml ]'%sys.argv[0]
-        exit(-1)
+    if len(sys.argv) < 1:
+        print 'usage : %s [ input.yaml ] output.yaml'%sys.argv[0]
 
-    inputfile = sys.argv[1]
+
     if len(sys.argv) > 2:
+        inputfile = sys.argv[1]
         outputfile = sys.argv[2]
     else:
-        outputfile = inputfile.replace('.yaml', '') + '_controller_config.yaml'
+        #outputfile = inputfile.replace('.yaml', '') + '_controller_config.yaml'
+        inputfile = ""
+        outputfile = sys.argv[1]
 
     of = open(outputfile, 'w')
+
+    print >> of, '##'
+    print >> of, '## auto generated file'
+    print >> of, '##'
+    print >> of, '##controller_configuration:'
+    print >> of, '##  - group_name: <joint group name>'
+    print >> of, '##    controller_name: <name of joint trajectory topic name>'
+    print >> of, '##    joint_list: ## list of using joints'
+    print >> of, '##      - <joint_name>'
+
+    if inputfile == "":
+        of.close()
+        exit(0)
 
     lst = yaml.load(open(inputfile).read())
 
@@ -24,15 +39,6 @@ if __name__ == '__main__':
         if l in limb_candidates:
             invalid_yaml = False
             break
-
-    print >> of, '##'
-    print >> of, '## auto generated file'
-    print >> of, '##'
-    print >> of, '##controller_configuration:'
-    print >> of, '##  - group_name: <joint group name>'
-    print >> of, '##    controller_name: <name of joint trajectory topic name>'
-    print >> of, '##    joint_list: ## list of using joints'
-    print >> of, '##      - <joint_name>'
 
     if invalid_yaml:
         of.close()
