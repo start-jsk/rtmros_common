4d640544a486ccc2034a6f349dc87c80378f40c4
Shunichi Nozawa <notheworld@gmail.com>
2014-06-24 13:22:00 +0900

    (compile_robot_model.cmake, hrpsys.launch, hrpsys_tools_config.py) Add argument to use Unstable RTC List and configure it from cmake discussed in https://github.com/start-jsk/rtmros_gazebo/pull/61

--- hrpsys_ros_bridge/cmake/compile_robot_model.cmake
+++ hrpsys_ros_bridge/cmake/compile_robot_model.cmake
@@ -570,10 +570,8 @@ macro (generate_default_launch_eusinterface_files wrlfile project_pkg_name)
   set(${_sname}_generated_launch_euslisp_files)
   #   generate hrpsys_config.py to use unstable RTCs
   if ("${ARGV3}" STREQUAL "--use-unstable-hrpsys-config")
-    configure_file(${hrpsys_ros_bridge_PACKAGE_PATH}/scripts/default_unstable_hrpsys_config.py.in ${PROJECT_SOURCE_DIR}/scripts/${_sname}_unstable_hrpsys_config.py)
-    list(APPEND ${_sname}_generated_launch_euslisp_files ${PROJECT_SOURCE_DIR}/scripts/${_sname}_unstable_hrpsys_config.py)
     set(ROSBRIDGE_ARGS "    <arg name=\"USE_WALKING\" default=\"true\" />\n    <arg name=\"USE_IMPEDANCECONTROLLER\" default=\"true\" />")
-    set(STARTUP_ARGS "    <arg name=\"HRPSYS_PY_PKG\" default=\"${PROJECT_PKG_NAME}\" />\n    <arg name=\"HRPSYS_PY_NAME\" default=\"${robot}_unstable_hrpsys_config.py\" />")
+    set(STARTUP_ARGS "    <arg name=\"HRPSYS_PY_ARGS\" default=\"--use-unstable-rtc\" />")
   endif()
   #  generate startup.launch
   configure_file(${hrpsys_ros_bridge_PACKAGE_PATH}/scripts/default_robot_startup.launch.in ${PROJECT_SOURCE_DIR}/launch/${_sname}_startup.launch)
