07843d0257d107d6b00734e15c42c5311a972766
Kei Okada <kei.okada@gmail.com>
2015-02-08 14:54:18 +0900

    update to use wstool and change script name

---
+++ setup_upstream.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+function usage {
+    echo >&2 "usage: $0"
+    echo >&2 "          [-w|--workspace] workspace to set openrtm/openhrp/hrpsys repository,"
+    exit 0
+}
+
+function error {
+    usage
+    exit 1
+}
+trap error ERR
+
+# command line parse
+OPT=`getopt -o hw: -l help,workspace: -- $*`
+if [ $? != 0 ]; then
+    usage
+fi
+
+eval set -- $OPT
+while [ -n "$1" ] ; do
+    case $1 in
+	-h|--help) usage ;;
+	-w|--workspace) WORKSPACE=$2; shift 2;;
+	--) shift; break;;
+	*) echo "Unknown option($1)"; usage;;
+    esac
+done
+
+if [ ! -e $WORKSPACE/src/.rosinstall ]; then
+    echo "Your workspace is not initialized yet, please run
+mkdir -p $WORKSPACE/src; wstool init $WORKSPACE/src
+"
+    exit 1
+fi
+
+for pkg in rtshell rtctree rtsprofile openrtm_aist openrtm_aist_python; do
+    cat <<EOF >> /tmp/rosinstall.$$
+- git:
+    uri: http://github.com/tork-a/$pkg-release
+    local-name: $pkg
+EOF
+done
+
+# update HISTORY-{en,ja}.txt, CMakeLists.txt(OPENHRP_VERSION) and ask kanehiro-san for set tag
+cat <<EOF >> /tmp/rosinstall.$$
+- git:
+    uri: http://github.com/fkanehiro/openhrp3
+    local-name: openhrp3
+EOF
+
+# update package.xml, CMakeLists.txt(CMAKE_PACKAGE_VERSION) and ask kanehiro-san for set tag
+cat <<EOF >> /tmp/rosinstall.$$
+- git:
+    uri: http://github.com/fkanehiro/hrpsys-base
+    local-name: hrpsys
+EOF
+
+wstool merge /tmp/rosinstall.$$ -t $WORKSPACE/src
+wstool info -t $WORKSPACE/src
+
+echo "*** Done"
+
