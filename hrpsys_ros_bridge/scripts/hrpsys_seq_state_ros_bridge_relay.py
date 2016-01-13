0a29d1cb96fe3df3573474039b027ed188c99b53
iori <iori@jsk.imi.i.u-tokyo.ac.jp>
2016-01-13 19:43:19 +0900

    [hrpsys_ros_bridge] Add HrpsysSeqStateROSBridge tf rate test

---
+++ hrpsys_ros_bridge/scripts/hrpsys_seq_state_ros_bridge_relay.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+import rospy
+import tf
+from tf.msg import tfMessage
+
+class HrpsysSeqStateROSBridgeTFRelay:
+    def __init__(self):
+        rospy.init_node("HrpsysSeqStateROSBridgeTFRelay", anonymous=True)
+        rospy.Subscriber('/tf', tfMessage, self.tf_callback)
+        self.broadcaster = rospy.Publisher("/hrpsys_ros_bridge/tf", tfMessage, queue_size = 100)
+
+    def execute(self):
+        while not rospy.is_shutdown():
+            rospy.spin()
+
+    def tf_callback(self, msg):
+        if msg._connection_header['callerid'] == '/HrpsysSeqStateROSBridge':
+            self.broadcaster.publish(msg)
+                
+if __name__ == '__main__':
+    try:
+        node = HrpsysSeqStateROSBridgeTFRelay()
+        node.execute()
+    except rospy.ROSInterruptException: pass
