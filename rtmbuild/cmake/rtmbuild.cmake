603b6a7cd76d17a0dbd874379f8531970e1cc930
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2019-07-30 14:54:41 +0900

    since catkin > 0.7.0, the CPATH is no longer being set by catkin, so rtmbuild manually add them

--- rtmbuild/cmake/rtmbuild.cmake
+++ rtmbuild/cmake/rtmbuild.cmake
@@ -134,6 +134,13 @@ macro(rtmbuild_init)
   endif()
 
   include_directories(${catkin_INCLUDE_DIRS} ${openrtm_aist_INCLUDE_DIRS} ${openhrp3_INCLUDE_DIRS})
+  # since catkin > 0.7.0, the CPATH is no longer being set by catkin, so rtmbuild manually add them
+  set(_cmake_prefix_path_tmp $ENV{CMAKE_PREFIX_PATH})
+  string(REPLACE ":" ";" _cmake_prefix_path_tmp ${_cmake_prefix_path_tmp})
+  foreach(_cmake_prefix_path ${_cmake_prefix_path_tmp})
+    include_directories(${_cmake_prefix_path}/include)
+  endforeach()
+
   link_directories(${catkin_LIBRARY_DIRS} ${openrtm_aist_LIBRARY_DIRS} ${openhrp3_LIBRARY_DIRS})
 
 endmacro(rtmbuild_init)
