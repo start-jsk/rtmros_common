12d3007a5e6b4d19d46278444732c830efeef007
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-12-17 11:15:55 +0000

    add debug node

---
+++ openrtm_tools/scripts/rtmlaunch2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from openrtm_tools import rtmlaunch
+
+import signal, sys
+def signal_handler(signum, frame):
+    sigdict = dict((k, v) for v, k in signal.__dict__.iteritems() if v.startswith('SIG'))
+    print >>sys.stderr, "\033[33m[rtmlaunch] Catch signal %r, exitting...\033[0m"%(sigdict[signum])
+    sys.exit(0)
+
+if __name__ == '__main__':
+    if '--verbose' in sys.argv:
+        print >>sys.stderr, "[rtmlaunch.py] {} starting... ".format(__file__)
+        print >>sys.stderr, "[rtmlaunch.py] loading {}... ".format(rtmlaunch)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    rtmlaunch.main()
+
+
+
+
